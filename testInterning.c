a0f77d6edc20740b0f2a3422ce5d0e39a1c869cc
qwe661234 <qwe661234@gmail.com>
2021-03-31 21:18:56 +0800

    Auto stash before checking out "HEAD"

--- testInterning.c
+++ testInterning.c
@@ -7,18 +7,9 @@ void testfunction(xs prefix, xs suffix, xs str1){
 }
 int main(int argc, char **argv){
     clock_t total = 0;
-    srand(time(0));
-    for(int j = 0; j < 1000; j++){
-        char *str = malloc(sizeof(char) * 21);
-        for(int i = 0; i < 20; i++){
-            int a = rand() % 58 + 65;
-            str[i] = (char)a;
-        }
-        str[20] = 0;
-        xs str1;
+    for(int j = 0; j < 100000; j++){
+        xs str1 = *xs_tmp("SAdasdsasdasdnlkan");
         xs prefix = *xs_tmp("((("), suffix = *xs_tmp(")))");
-        str1.ptr = str;
-        str1.is_ptr = 1;
         TICK(start_t);
         testfunction(prefix, suffix, str1);
         total += TOCK(start_t); 
